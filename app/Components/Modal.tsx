62615f0becc1ac1e46466d2536d6df5b63fbab14
VishwRana-Coder <vishw.rana.k@gmail.com>
2024-10-24 20:05:28 +0530

    some changes

--- app/Components/Modal.tsx
+++ app/Components/Modal.tsx
@@ -1,18 +1,17 @@
-"use client";
-
 import { Transition, Dialog } from "@headlessui/react";
 import { Fragment, useState } from "react";
 import { IoClose } from "react-icons/io5";
 import { Button as AntButton, Modal as AntModal } from 'antd';
+import React from "react";
 
 interface CustomModalProps {
     isOpen: boolean;
     onClose: () => void;
     children: React.ReactNode;
-    buttonProp: React.ReactNode
+    buttonProp: React.ReactNode;
 }
 
-const  CustomModal: React.FC<CustomModalProps> = ({
+const CustomModal: React.FC<CustomModalProps> = ({
     isOpen,
     onClose,
     children,
@@ -20,12 +19,24 @@ const  CustomModal: React.FC<CustomModalProps> = ({
 }) => {
     return (
         <div>
-            <AntModal open={isOpen} onOk={onClose} onCancel={onClose} centered footer={[
-                <div className="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
-                    {buttonProp}
-                </div>
-            ]
-            }>
+            <AntModal 
+                open={isOpen} 
+                onOk={onClose} 
+                onCancel={onClose} 
+                centered 
+                footer={[
+                    <div key="footer" className="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
+                        {React.Children.toArray(buttonProp).map((child, index) => {
+                            // Ensure the child is a valid React element
+                            if (React.isValidElement(child)) {
+                                return React.cloneElement(child, { key: index });
+                            }
+                            return null; // Return null for invalid children
+                        })}
+                    </div>
+                ]}
+
+            >
                 {children}
             </AntModal>
         </div>
