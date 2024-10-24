e9e4a8fbdf8fdf974dc7953db0000d8352515afd
VishwRana-Coder <vishw.rana.k@gmail.com>
2024-10-24 20:35:38 +0530

    less changes

--- app/actions/getSession.ts
+++ app/actions/getSession.ts
@@ -1,6 +1,6 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/route";
+import { authOptions } from "../api/auth/[...nextauth]/route"; // This path should match your structure
 
-export default async function getSession(){
+export default async function getSession() {
     return await getServerSession(authOptions);
 }
