97418924054af5cada788ae8af82bcc494f8a3e8
VishwRana-Coder <vishw.rana.k@gmail.com>
2024-10-24 20:42:38 +0530

    final change

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,12 +1,13 @@
-import bcrypt from 'bcrypt';
-import NextAuth, { NextAuthOptions } from 'next-auth';
-import CredentialsProvider from 'next-auth/providers/credentials';
-import GithubProvider from 'next-auth/providers/github';
-import GoogleProvider from 'next-auth/providers/google';
-import { PrismaAdapter } from '@next-auth/prisma-adapter';
-import prisma from '@/app/libs/prismadb';
+import bcrypt from 'bcrypt'
+import NextAuth, { AuthOptions} from 'next-auth'
+import CredentialsProvidders from 'next-auth/providers/credentials'
+import GithubProvider from 'next-auth/providers/github'
+import GoogleProvider from 'next-auth/providers/google'
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
 
-export const authOptions: NextAuthOptions = {
+import prisma from '@/app/libs/prismadb'
+
+export const authOptions: AuthOptions = {
     adapter: PrismaAdapter(prisma),
     providers: [
         GithubProvider({
@@ -17,14 +18,14 @@ export const authOptions: NextAuthOptions = {
             clientId: process.env.GOOGLE_CLIENT_ID as string,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
         }),
-        CredentialsProvider({
-            name: "Credentials",
+        CredentialsProvidders({
+            name: "creddentials",
             credentials: {
-                email: { label: 'Email', type: 'text' },
-                password: { label: 'Password', type: 'password' },
+                email: {label: 'email', type: 'text'},
+                password: {label: "password", type: "password"},
             },
-            async authorize(credentials) {
-                if (!credentials?.email || !credentials?.password) {
+            async authorize(credentials){
+                if(!credentials?.email || !credentials?.password){
                     throw new Error("Invalid Credentials");
                 }
                 
@@ -34,16 +35,15 @@ export const authOptions: NextAuthOptions = {
                     }
                 });
 
-                if (!user || !user?.hashedPassword) {
-                    throw new Error("Invalid Credentials");
+                if(!user || !user?.hashedPassword){
+                    throw new Error("Invalid Credentials")
                 }
-                
                 const isCorrectPassword = await bcrypt.compare(
                     credentials.password,
                     user.hashedPassword
                 );
 
-                if (!isCorrectPassword) {
+                if(!isCorrectPassword){
                     throw new Error("Invalid Credentials");
                 }
 
@@ -51,16 +51,13 @@ export const authOptions: NextAuthOptions = {
             }
         })
     ],
-    pages: {
-        signIn: '/auth/signin',
-        signOut: '/auth/signout',
-        error: '/auth/error',
-        verifyRequest: '/auth/verify-request',
-        newUser: '/auth/new-user',
-    },
     debug: process.env.NODE_ENV === "development",
     session: {
         strategy: "jwt",
     },
     secret: process.env.NEXTAUTH_SECRET,
-};
+}
+
+const handler = NextAuth(authOptions);
+
+export {handler as GET, handler as POST};
